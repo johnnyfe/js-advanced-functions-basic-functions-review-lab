903f62bbe82e3948b4a8ef3e8e72415cb35749ac
johnny <johnny1194@hotmail.com>
2021-04-03 11:56:12 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+function saturdayFun (activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork (activity1='go to the office'){;
+    return`This Monday, I will ${activity1}.` ;
+}
+mondayWork (`work from home`);
+let wrapAdjective = function (style="*") {
+    return function (adj='a hard worker'){
+        return `You are ${style}${adj}${style}!`
+    };
+}
+
+const Calculator = {
+    add:function(a,b){
+        return a+b;
+    },
+    subtract:function(a,b){
+        return a-b;
+    },
+    multiply:function(a,b){
+        return a*b;
+    },
+    divide:function(a,b){
+        return a/b;
+    }
+
+}
+let actionApplyer = function(start, ray) {
+    let a = start
+  
+    for (let i = 0; i < ray.length; i++ ){
+      a = ray[i](a)
+    }
+  
+    return a
+  }
